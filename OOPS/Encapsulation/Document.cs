5ebcc7f39602812bae56cb0fa9ec20f7bd38bbb5
Mehta <127237579+VarunPractice@users.noreply.github.com>
2024-04-10 03:46:27 +0530

    Add files via upload
    Creating Repo for OOPS

---
+++ OOPS/Encapsulation/Document.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics.OOPS.Encapsulation
+{
+    public class Document
+    {
+        private string DocName { get; set; }
+        private string DocType { get; set; }
+        = string.Empty;
+        private string Content { get; set; }
+
+        public Document(string name, string type, string content)
+        {
+            DocName = name;
+            DocType = type;
+            Content = content;
+        }
+
+        public string GetDocument()
+        {
+            Console.WriteLine("doc returned :" + DocName); ;
+            return DocName; }
+        public string SetDocument(string newvalue)
+        {
+            Console.WriteLine("doc updated from old value : " + DocName + " with new value" + newvalue); 
+            return DocName= newvalue; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Basics.OOPS.Encapsulation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    internal class Program
+    {
+     static   Document document;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Doc Name");
+            var name = Console.ReadLine();
+            Console.WriteLine("Enter Doc Type");
+            var type  = Console.ReadLine();
+            Console.WriteLine("Enter Doc Content");
+            var content = Console.ReadLine();
+            document = new Document(name, type, content);
+            document.GetDocument();
+            document.SetDocument("jai shree Ram");
+        }
+    }
+}
