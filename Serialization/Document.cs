e88b1c6125852932812602b2eb69783fb86d63cf
Aarush@122015 <you@example.com>
2024-04-10 05:45:52 +0530

    Added Serialization/Deserialization

---
+++ Serialization/Document.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics.Serialization
+{
+    [Serializable]
+    public class Document
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public Document(string title, string content)
+        {
+            Title = title;
+            Content = content;
+        }
+    }
+}
